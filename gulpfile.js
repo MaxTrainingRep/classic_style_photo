dfaa37f670830d3a11d1bb9f40d46a78f99e1065
MaxTrainingRep <mxgryn@gmail.com>
2022-10-07 08:45:00 +0300

    cheked

--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,7 @@ const webpack = require("webpack-stream");
 const browsersync = require("browser-sync");
 
 // const dist = "./dist/";
-const dist = "C:/MAMP/htdocs/test";
+const dist = "C:/MAMP/htdocs/test2";
 
 gulp.task("copy-html", () => {
     return gulp.src("./src/index.html")
